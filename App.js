b555b88077817921c42bbe6c46115a4620455385
Rustem Dzhuraiev <73857551+RustDzhur@users.noreply.github.com>
2022-12-19 17:34:17 +0200

    initial files

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
+// import bg from './assets/bg.jpg'
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<ImageBackground source={require('./assets/bg.jpg')} style={styles.bg}>
+				<Text style={styles.text}>
+					Open up App.js to start working on your app?
+				</Text>
+				<StatusBar style="auto" />
+			</ImageBackground>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	text: {
+		color: "green",
+		fontSize: 35,
+		textAlign: "center",
+	},
+	bg: {
+		flex: 1,
+		resizeMode: 'cover',
+		justifyContent: 'center',
+		width: '100%',
+	}
 });
